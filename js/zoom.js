4cc9504f03b49363542b95cacf9e145df1b1accd
liam.k@live.no <Liam.k@live.no>
2018-06-19 01:23:21 +0200

    added key and swipe event to change image

--- js/zoom.js
+++ js/zoom.js
@@ -9,29 +9,29 @@ var firstInit = true;
 var divGird;
 var lastImgLoaded = 7;
 var done = false;
-var imgNeedToLoad = 8;
+var imgNeedToLoad = 0;
 var justChanged = false;
 var lastZoomedImgRoom = null;
 var lastZoomedImgNum = 0;
 var ready1 = false;
+var timer = false;
 
+//Zoom Functions
 function nextImg(i){
     if (lastZoomedImgRoom == null) return;
     ZoomOutControll();
-    if(lastZoomedImgNum == numOfImg && i > 0) lastZoomedImgNum = 0;
-    else if (lastZoomedImgNum == 0 && i < 0) lastZoomedImgNum = numOfImg;
+    if(lastZoomedImgNum == (numOfImg-1) && i > 0) lastZoomedImgNum = 0;
+    else if (lastZoomedImgNum == 0 && i < 0) lastZoomedImgNum = numOfImg-1;
     else lastZoomedImgNum = lastZoomedImgNum + i;
     document.getElementById('zoomImg').src = './img/' + roomName + '/' + lastZoomedImgNum + '.jpg';
     
 }
-
 function ZoomOutControll() {
     justChanged = true;
         setTimeout(function () {
             justChanged = false;
         }, 300); 
 }
-
 function zoomIn(roomName, i) {
     document.getElementById("body").style.overflow = 'hidden';
     document.getElementById('zoomImg').src = './img/' + roomName + '/' + i + '.jpg';
@@ -40,15 +40,67 @@ function zoomIn(roomName, i) {
     lastZoomedImgNum = i;
     lastZoomedImgRoom = roomName;
 }
-
 function zoomOut() {
     if(justChanged || !inZoom) return;
-    document.getElementById("body").style.overflow = 'auto';
+    if(!toucheExit ) document.getElementById("body").style.overflow = 'auto';
+    else{
+        setTimeout(function () {
+            document.getElementById("body").style.overflow = 'auto';
+            toucheExit = false;
+        }, 400); 
+    }
     document.getElementById('imgBack').style.display = 'none';
     inZoom = false;
     lastZoomedImg = null;
 }
+//image change on key or swipe
+window.onkeyup = function(e) {
+    if(!inZoom) return;
+    var key = e.keyCode ? e.keyCode : e.which;
+    
+    if (key == 39 || key == 13)nextImg(1);
+    else if (key == 37) nextImg(-1);
+    else if(key == 27) zoomOut();
+ }; 
 
+ document.addEventListener('touchstart', handleTouchStart, false);        
+ document.addEventListener('touchmove', handleTouchMove, false);
+ 
+ var xDown = null;                                                        
+ var yDown = null;                                                        
+ var toucheExit = false;
+ function handleTouchStart(evt) {                                         
+     xDown = evt.touches[0].clientX;                                      
+     yDown = evt.touches[0].clientY;                                      
+ };                                                
+ 
+ function handleTouchMove(evt) {
+     if ( ! xDown || ! yDown ) {
+         return;
+     }
+ 
+     var xUp = evt.touches[0].clientX;                                    
+     var yUp = evt.touches[0].clientY;
+ 
+     var xDiff = xDown - xUp;
+     var yDiff = yDown - yUp;
+ 
+     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+         if ( xDiff > 0 ) {
+             nextImg(1);
+         } else {
+             nextImg(-1);
+         }                       
+     } else {
+         toucheExit = true;
+         zoomOut();                                                                
+     }
+     /* reset values */
+     xDown = null;
+     yDown = null;                                             
+ };
+
+//loading Images functions
 function imageLoaded(){
     imgNeedToLoad--;
     if(imgNeedToLoad < 1){
@@ -56,19 +108,15 @@ function imageLoaded(){
         document.getElementById('loeadingImg').style.display = 'none';
     }
 }
-
 function ready(){
     ready1 = true;
 }
-
 function reTryInit(){
     setTimeout(function () {
         timer = false;
         init();
     }, 100);
 }
-
-var timer = false;
 function init() {
     //console.log("roomName: "+ roomName);
     //console.log("lastRoom: "+ lastRoom);
@@ -100,13 +148,15 @@ function read() {
     }
 }
 
+//Scroll functions
 function scrollUpdate() {
     if(firstInit) return;
     //title resize
     document.getElementById('solias').style.fontSize = (Math.max(130 - getScrollTop(), 70) + 'px');
     document.getElementById('boligstyling').style.fontSize = (Math.max(50 - getScrollTop()/2, 20) + 'px');
     
-    if(imgNeedToLoad > 0) {
+    //image loading
+    if(imgNeedToLoad > 0 && !done) {
         document.getElementById('loeadingImg').style.display = 'inherit';
         return;
     }
