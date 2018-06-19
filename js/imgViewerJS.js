e834d9d4a62cae465c43dc2ecc05714319e64dbf
liam.k@live.no <Liam.k@live.no>
2018-06-19 02:43:59 +0200

    updated imageViewer

--- js/imgViewerJS.js
+++ js/imgViewerJS.js
@@ -3,11 +3,6 @@
  */
 
 /**----------------------------------------------------------------*/
-
-//update this with number of images
-//image names {0.jpg .. n.jpg}
-var numberOfImg = 45;
-
 //how fast the image fade, bigger number will make the animation to go faster.
 //smaller number vil make the animation to take longer.
 var AnimationTime = 0.01;
@@ -15,27 +10,64 @@ var AnimationTime = 0.01;
 //how long each image will stay om. in millie second. 3000 = 3 sec
 var imageViewTime = 3000;
 
-
-//don't change anything farther down!!
 //----------------------------------------------------------------//
-/**---------------------------------------------------------------*/
+//----------------------------------------------------------------//
+var numberOfImg = 8;
 var lastLoaded = 0;
+var doneloading = 0;
 var lastShown = 0;
 var play = true;
 var firstInit = true;
+var ready1 = false;
 
 function init() {
-    if(firstInit){
-        load(1);
-        firstInit = false;
-        if(play) goPlay();
+    if(!firstInit) return;
+    if(!ready1){
+        timer = true;
+        reTryInit();
+        return;
+    }
+    numberOfImg = read();
+    load(1);
+    firstInit = false;
+    if(play) goPlay();   
+}
+function reTryInit(){
+    setTimeout(function () {
+        timer = false;
+        init();
+    }, 100);
+}
+function ready(){
+    ready1 = true;
+}
+function read() {
+    var oFrame = document.getElementById("fromFile");
+    var text = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
+    var rooms = text.split("|");
+    for (var i = 0; i< rooms.length; i++ ){
+        if(rooms[i].indexOf(roomName) > -1) {
+            var thisRoom = rooms[i].split(":");
+            return parseInt(thisRoom[1]);
+        }
     }
 }
+function imageloaded(){
+    doneloading++;
+}
+
 
 function load(i){
+    if(lastLoaded > doneloading + 3) {
+        setTimeout(function () {
+            load(i);
+            return;
+        }, 50);
+        return;
+    }
     var imgHtml = "";
-    while(i < lastShown + 7 && i <= numberOfImg){
-        imgHtml += "<img src='./imgViewerImages/" + i + ".jpg' id='Image" + i + "' class='mainImage' style='display: none'>";
+    while(i < lastShown + 7 && i < numberOfImg){
+        imgHtml += "<img src='./imgViewerImages/" + i + ".jpg' onload='imageloaded()' id='Image" + i + "' class='mainImage' style='display: none'>";
         lastLoaded = i++;
     }
     document.getElementById("imgDiv").innerHTML += imgHtml;
@@ -43,17 +75,15 @@ function load(i){
 
 function changeImg() {
     var view = lastShown + 1;
-    if(view > lastLoaded){
-        view = 0;
-    }
+    if(view == numberOfImg) view = 0;
+    if(view+1 > doneloading) return;
+    
     var vi = document.getElementById("Image" + view);
     vi.style.display = 'inherit';
     vi.style.zIndex = 100;
-    if(play){
-        changeImgAnimation(lastShown, 1.0);
-    }else{
-        document.getElementById("Image" + lastShown).style.display = 'none';
-    }
+    
+    changeImgAnimation(lastShown, 1.0);
+    
     lastShown = view;
     if(lastShown > lastLoaded - 6 && numberOfImg != lastLoaded) load(lastLoaded + 1);
 }
@@ -61,14 +91,12 @@ function changeImg() {
 function changeImgAnimation(p, o) {
     var pre = document.getElementById("Image" + p);
     pre.style.zIndex = 200;
-    //pre.style.position = "absolute";
     setTimeout(function () {
         if(o > 0.001){
             pre.style.opacity = o;
             changeImgAnimation(p, o - AnimationTime);
         }else{
             pre.style.display = 'none';
-            //pre.style.position = 'inherit';
             pre.style.opacity = 1.0;
         }
     }, 10);
